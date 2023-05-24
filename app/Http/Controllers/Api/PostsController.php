fd9f187145b2ad2e0d52110ddefde00b9d896481
bayanga antoine <badinfo1987@gmail.com>
2023-05-24 11:01:26 +0100

    custom middleware to verify the token

--- app/Http/Controllers/Api/PostsController.php
+++ app/Http/Controllers/Api/PostsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 
 class PostsController extends Controller
 {
--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,9 @@ use App\Http\Controllers\Api\PostsController;
 use App\Http\Controllers\Api\CommentsController;
 use App\Http\Controllers\Api\LikesController;
 use App\Http\Controllers\Api\PaiementsController;
-use App\Http\Controllers\Api\CommandesController;/*
+use App\Http\Controllers\Api\CommandesController;
+use App\Http\Middleware\VerifyJWTToken;
+/*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
@@ -24,28 +26,28 @@ Route::post('/login',[AuthController::class,'login']);
 Route::get('logout', [AuthController::class, 'logout']);
 
 //post
-Route::post('posts/create', [PostsController::class, 'create'])->middleware('JWTAuth');
-Route::post('posts/delete', [PostsController::class, 'delete'])->middleware('JWTAuth');
-Route::post('posts/update', [PostsController::class, 'update'])->middleware('JWTAuth');
-Route::get('posts', [PostsController::class, 'posts'])->middleware('JWTAuth');
+Route::post('posts/create', [PostsController::class, 'create'])->middleware(VerifyJWTToken::class);
+Route::post('posts/delete', [PostsController::class, 'delete'])->middleware(VerifyJWTToken::class);
+Route::post('posts/update', [PostsController::class, 'update'])->middleware(VerifyJWTToken::class);
+Route::get('posts', [PostsController::class, 'posts'])->middleware(VerifyJWTToken::class);
 
 //comments
-Route::post('comments/create', [CommentsController::class, 'create'])->middleware('JWTAuth');
-Route::post('comments/delete', [CommentsController::class, 'delete'])->middleware('JWTAuth');
-Route::post('comments/update', [CommentsController::class, 'update'])->middleware('JWTAuth');
-Route::post('posts/comments', [CommentsController::class, 'comments'])->middleware('JWTAuth');
+Route::post('comments/create', [CommentsController::class, 'create'])->middleware(VerifyJWTToken::class);
+Route::post('comments/delete', [CommentsController::class, 'delete'])->middleware(VerifyJWTToken::class);
+Route::post('comments/update', [CommentsController::class, 'update'])->middleware(VerifyJWTToken::class);
+Route::post('posts/comments', [CommentsController::class, 'comments'])->middleware(VerifyJWTToken::class);
 
 //like
-Route::post('posts/like', [LikesController::class, 'like'])->middleware('JWTAuth');
+Route::post('posts/like', [LikesController::class, 'like'])->middleware(VerifyJWTToken::class);
 
 //paiements
-Route::post('paiements/create', [PaiementsController::class, 'create'])->middleware('JWTAuth');
-Route::post('paiements/delete', [PaiementsController::class, 'delete'])->middleware('JWTAuth');
-Route::post('paiements/update', [PaiementsController::class, 'update'])->middleware('JWTAuth');
-Route::get('commandes/paiements', [PaiementsController::class, 'commandes'])->middleware('JWTAuth');
+Route::post('paiements/create', [PaiementsController::class, 'create'])->middleware(VerifyJWTToken::class);
+Route::post('paiements/delete', [PaiementsController::class, 'delete'])->middleware(VerifyJWTToken::class);
+Route::post('paiements/update', [PaiementsController::class, 'update'])->middleware(VerifyJWTToken::class);
+Route::get('commandes/paiements', [PaiementsController::class, 'commandes'])->middleware(VerifyJWTToken::class);
 
 //commandes
-Route::post('commandes/create', [CommandesController::class, 'create'])->middleware('JWTAuth');
-Route::post('commandes/delete', [CommandesController::class, 'delete'])->middleware('JWTAuth');
-Route::post('commandes/update', [CommandesController::class, 'update'])->middleware('JWTAuth');
-Route::post('commandes', [CommandesController::class, 'commandes'])->middleware('JWTAuth');
+Route::post('commandes/create', [CommandesController::class, 'create'])->middleware(VerifyJWTToken::class);
+Route::post('commandes/delete', [CommandesController::class, 'delete'])->middleware(VerifyJWTToken::class);
+Route::post('commandes/update', [CommandesController::class, 'update'])->middleware(VerifyJWTToken::class);
+Route::post('commandes', [CommandesController::class, 'commandes'])->middleware(VerifyJWTToken::class);
